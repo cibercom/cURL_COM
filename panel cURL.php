a1cf0c7ab946f83d65218418aa1494a2119e6ca1
cibercom <osvaldito.almonte.2030@gmail.com>
2026-04-06 19:39:39 -0400

    Add files via upload

--- panel cURL.php
+++ panel cURL.php
@@ -7,24 +7,27 @@
     </head>
     <body>
 
-    ?php
+<?php
 
-   $ch1 = curl_init("https://prm.org.do/");
-   $ch2 = curl_init("https://whatsapp.com/channel/0029Va8T7MODZ4LWDjAe012c");
-   $ch3 = curl_init("https://whatsapp.com/channel/0029Va8EU026rsQmtPyLvp2I");
-   
+$url = "https://prm.org.do/"; 
 
-   curl_multi_remove_handle($mh, $ch1);
-   curl_multi_remove_handle($mh, $ch2);
-   curl_multi_remove_handle($mh, $ch3);
-   
-  
-   curl_close($ch1);
-   curl_close($ch2);
-   curl_close($ch3);
-   curl_multi_close($mh);
+$ch = curl_init($url);
 
-    ?>
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE"); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   
+
+$response = curl_exec($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+if ($http_code == 200) {
+  echo "Resource deleted successfully!";
+} else {
+  echo "Error deleting resource: " . $http_code;
+}
+
+?>
 	
     </body>
 </html>		
